ef44dabaadb926943497010c2cee6d778147c85e
doug <doug@mercury.(none)>
2012-10-10 00:03:38 -0400

    Improve synopsis, docs

--- lib/String/Compare/ConstantTime.pm
+++ lib/String/Compare/ConstantTime.pm
@@ -29,6 +29,14 @@ String::Compare::ConstantTime - Timing side-channel protected string compare
 =head1 SYNOPSIS
 
     use String::Compare::ConstantTime;
+
+    if (String::Compare::ConstantTime::equals($secret_data, $user_supplied_data)) {
+      ## The strings are eq
+    }
+
+An example with HMACs:
+
+    use String::Compare::ConstantTime;
     use Digest::HMAC_SHA1; ## or whatever
 
     my $hmac_ctx = Digest::HMAC_SHA1->new($key);
@@ -47,7 +55,7 @@ This module provides one function, C<equals> (not exported by default).
 
 You should pass this function two strings of the same length. It will return true if they are string-wise identical and false otherwise, just like C<eq>. However, comparing any two differing strings will take a fixed amount of time, unlike C<eq>.
 
-B<NOTE>: If the lengths of the strings are different, C<equals> will return false right away. Also, comparing two identical strings will take a different amount of time than comparing two differing strings.
+If the lengths of the strings are different, C<equals> will return false right away.
 
 
 
@@ -57,6 +65,8 @@ Some programs take different amounts of time to run depending on the input value
 
 Most routines that compare strings (like perl's C<eq> and C<cmp> and C's C<strcmp> and C<memcmp>) start scanning from the start and terminate as soon as they determine the strings won't match. This is good for efficiency but bad because it opens a timing side-channel. If one of the strings being compared is a secret and the other is controlled by some untrusted party, it is sometimes possible for this untrusted party to learn the secret using a timing side-channel.
 
+If the lengths of the strings are different, because C<equals> returns false right away the size of the secret string may be leaked (but not its contents).
+
 
 
 =head1 HMAC
@@ -65,11 +75,13 @@ HMACs are "Message Authentication Codes" built on top of cryptographic hashes. T
 
 To verify a candidate digest included with a message, you re-compute the digest using the message and the secret password. If this computed digest is is the same as the candidate digest then the message is considered authenticated.
 
-A very common side-channel attack against services that verify unlimited numbers of messages automatically is to create a forged message and then just send some random junk as the candidate digest. Continue sending this message and the junk digest, varying the first character in the digest. Repeat many times. If you find a particular digest that statistically takes a longer time to be rejected than the other digests, it is probably because this particular digest has the first character correct and the service's final string comparison is running a little longer.
+A common side-channel attack against services that verify unlimited numbers of messages automatically is to create a forged message and then just send some random junk as the candidate digest. Continue sending this message and the junk digest, varying the first character in the digest. Repeat many times. If you find a particular digest that statistically takes a longer time to be rejected than the other digests, it is probably because this particular digest has the first character correct and the service's final string comparison is running a little longer.
 
 At this point, you keep this first character fixed and start varying the second character. Repeat until all the characters are solved or until the amount of remaining possibilities are so small you can brute force it. At this point, your candidate digest is considered valid and you have forged a message.
 
-Note that this particular attack doesn't allow the attacker to recover the secret input key, but the attacker can still use the service itself to produce a valid digest for any message.
+Note that this particular attack doesn't allow the attacker to recover the secret input key to the HMAC, but nevertheless can produce a valid digest for any message given enough time.
+
+B<NOTE>: Although this module protects against a common attack, it is in no way endorsement for the stateless storage of sessions in browser cookies and such.
 
 
 
@@ -77,9 +89,9 @@ Note that this particular attack doesn't allow the attacker to recover the secre
 
 Pin tumbler locks are susceptible to being picked in a similar way to an attacker forging HMAC digests using a timing side-channel.
 
-The most common way to pick cheap pin tumbler locks is to apply torque to the lock cylinder so that the pins are pressed against the cylinder. However, because of slight manufacturing discrepancies one particular pin will be the widest by a slight margin and will actually be the only pin pressed against the cylinder (the cheaper the lock, the higher the manufacturing "tolerances"). The attacker lifts this pin until the cylinder gives a little bit, indicating that this pin has been solved and the next widest pin is now the one being pressed against the cylinder. This process is repeated until all the pins are solved and the lock opens.
+The traditional way to pick cheap pin tumbler locks is to apply torque to the lock cylinder so that the pins are pressed against the cylinder. However, because of slight manufacturing discrepancies one particular pin will be the widest by a slight margin and will actually be the only pin pressed against the cylinder (the cheaper the lock, the higher the manufacturing tolerances). The attacker lifts this pin until the cylinder gives a little bit, indicating that this pin has been solved and the next widest pin is now the one being pressed against the cylinder. This process is repeated until all the pins are solved and the lock opens.
 
-Just like an attacker trying to solve HMAC digests can work on one character at a time, a lock pick can work on each pin in isolation. To protect against this, quality locks force all pins to be fixed into place before the cylinder rotation can begin just as secure HMAC verifiers force attackers to guess the entire digest on each attempt.
+Just like an attacker trying to solve HMAC digests can work on one character at a time, a lock pick can work on each pin in isolation. To protect against this, quality locks force all pins to be fixed into place before the cylinder rotation can be attempted, just as secure HMAC verifiers force attackers to guess the entire digest on each attempt.
 
 
 
