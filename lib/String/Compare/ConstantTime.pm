43464fff448529ee4a121ea5db2cd6066b82c4b6
doug <doug@hcsw.org>
2015-10-24 17:50:42 -0400

    releasing 0.311

--- lib/String/Compare/ConstantTime.pm
+++ lib/String/Compare/ConstantTime.pm
@@ -2,7 +2,7 @@ package String::Compare::ConstantTime;
 
 use strict;
 
-our $VERSION = '0.310';
+our $VERSION = '0.311';
 
 require XSLoader;
 XSLoader::load('String::Compare::ConstantTime', $VERSION);
